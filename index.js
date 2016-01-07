1e2f0e3eba690a19ac06761a155f78a56327e941
andrea_cajamarca <andreac0893@tywls.org>
2016-01-07 21:43:10 +0000

    I added another noun in html and js.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function generateMadlib() {
   
   // This is the starting story
-  var story = "From the start, my vacation has been ADJECTIVE_1. I was VERB away from NOUN. Fortunately, I ACTION and it went away in a cloud of NOUN_2";
+  var story = "From the start, my vacation has been ADJECTIVE_1. I was VERB away from NOUN. Fortunately, I ACTION and it went away in a cloud of NOUN_2. I proceeded to talk to the magical NOUN_3";
   
 
   var inputValue = document.getElementById("adjective_1").value;
@@ -18,6 +18,9 @@ function generateMadlib() {
   
    inputValue = document.getElementById("noun_2").value;
   story = story.replace("NOUN_2", inputValue);
+  
+   inputValue = document.getElementById("noun_3").value;
+  story = story.replace("NOUN_3", inputValue);
   
   // This writes the story when the user clicks the "Generate MadLib" button"
   document.getElementById("result").innerHTML = story;
